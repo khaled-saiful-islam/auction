bf3ad3ab351a97623f797a7634f734bab8817363
Khaled Saiful Islam <saiful.islam@bjitgroup.com>
2017-05-23 11:13:56 +0600

    added db in DB folder

--- db/auction.sql
+++ db/auction.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 23, 2017 at 01:24 PM
+-- Generation Time: May 23, 2017 at 07:11 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -30,7 +30,6 @@ CREATE TABLE IF NOT EXISTS `bids` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `bid_time` timestamp NOT NULL,
   `bid_amount` int(11) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
@@ -64,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `categories`
@@ -73,7 +72,42 @@ CREATE TABLE IF NOT EXISTS `categories` (
 INSERT INTO `categories` (`id`, `name`, `created`, `modified`) VALUES
 (1, 'Computer Accessories', '2017-01-23 12:21:51', '2017-01-23 12:21:51'),
 (2, 'Home Appliance', '2017-01-23 12:22:44', '2017-01-23 12:22:44'),
-(3, 'Sports', '2017-01-23 12:23:23', '2017-01-23 12:23:23');
+(3, 'Sports', '2017-01-23 12:23:23', '2017-01-23 12:23:23'),
+(4, 'Camera', '2017-03-21 16:56:26', '2017-03-21 16:56:26'),
+(5, 'Vehicle', '2017-03-21 16:57:43', '2017-03-21 16:57:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `subject` varchar(128) DEFAULT NULL,
+  `message` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE IF NOT EXISTS `payments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `amount` int(11) DEFAULT NULL,
+  `payment_type` int(11) NOT NULL DEFAULT '1',
+  `user_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -86,6 +120,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `code` varchar(128) DEFAULT NULL,
   `title` varchar(256) NOT NULL,
   `details` varchar(1024) DEFAULT NULL,
+  `type` int(11) NOT NULL DEFAULT '1',
   `minimum_bid` int(11) DEFAULT '0',
   `minimum_increment` int(11) DEFAULT '0',
   `highest_bid` int(11) DEFAULT '0',
@@ -96,10 +131,13 @@ CREATE TABLE IF NOT EXISTS `products` (
   `image3_path` varchar(255) NOT NULL,
   `image4_path` varchar(255) NOT NULL,
   `created_by` int(11) NOT NULL,
+  `isPause` int(11) NOT NULL DEFAULT '0',
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
 
 -- --------------------------------------------------------
 
@@ -125,19 +163,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
   `level` int(11) NOT NULL DEFAULT '21',
+  `type` int(11) NOT NULL DEFAULT '1',
+  `payment` int(11) NOT NULL DEFAULT '0',
   `image_path` varchar(255) NOT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `level`, `image_path`, `created`, `modified`) VALUES
-(1, 'Khaled Saiful Islam', 'khaled_sfl@yahoo.com', '$2y$10$mC71iGlr.MzYoueQDoOmcOHIq3621JHfUhLy.jT2fvH2fx.6ACwou', 1, '', '2016-12-01 03:10:35', '2017-01-05 11:39:11');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `address`, `phone_number`, `level`, `type`, `payment`, `image_path`, `created`, `modified`) VALUES
+(1, 'Khaled Saiful Islam', 'khaled_sfl@yahoo.com', '$2y$10$mC71iGlr.MzYoueQDoOmcOHIq3621JHfUhLy.jT2fvH2fx.6ACwou', 'Dhaka, Bangladesh', '', 1, 1, 0, '', '2016-12-01 03:10:35', '2017-01-05 11:39:11');
 
 --
 -- Constraints for dumped tables
--- db/products_categories.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products_categories (
-    product_id INT NOT NULL,
-    category_id INT NOT NULL,
-    PRIMARY KEY (product_id, category_id),
-    FOREIGN KEY category_key(category_id) REFERENCES categories(id),
-    FOREIGN KEY product_key(product_id) REFERENCES products(id)
-);
